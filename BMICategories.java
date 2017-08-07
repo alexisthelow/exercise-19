0f97fcc71d6e037015c555601142adaaa2fdf56f
Alexis Low <alexisthelow@gmail.com>
2017-08-06 20:33:29 -0600

    second commit

--- BMICategories.java
+++ BMICategories.java
@@ -2,11 +2,24 @@ import java.util.Scanner;
 
 public class BMICategories {
 	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		double bmi;
+		Scanner keyboard = new Scanner(System.in);		
+		double ft, in, lbs, m, kg, bmi;
 		
-		System.out.print("Enter your BMI: ");
-		bmi = keyboard.nextDouble();
+		System.out.print("Your height (feet only): ");
+		ft = keyboard.nextDouble();
+		
+		System.out.print("Your height (inches only): ");
+		in = (keyboard.nextDouble() + (ft * 12));
+		
+		System.out.print("Your weight in pounds: ");
+		lbs = keyboard.nextDouble();
+		
+		m = (in * 0.0254);
+		kg = (lbs * 0.4535);
+		
+		bmi = kg / (m*m);
+		
+		System.out.println("Your BMI is " + bmi + ".");	
 		
 		System.out.print("BMI category: ");
 		if (bmi < 15) {
